c8dde9d68dc8f912364fc59d8e3e78dcd39837bc
Gawah Chan <gawahchan@gmail.com>
2019-07-31 19:46:10 +0100

    initital commit

--- backend/src/controllers/ping.ts
+++ backend/src/controllers/ping.ts
@@ -2,6 +2,12 @@ import * as express from "express";
 
 export const pingController = express.Router();
 
+pingController.get('/', (_, res) => {
+  res.status(200).json({
+    test: 'Hello World!'
+  })
+});
+
 pingController.get('/hello', (_, res) => {
   res.status(200).json({
     greetings: 'Thank you for spending some time on this test. All the best 🙌'
